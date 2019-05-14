936e85209d43890f37c1b7ea323cd480e56ff91a
Matt <carlotta.matt@gmail.com>
2019-05-13 17:56:26 -0700

    Remove node_modules tracking and switch to storing theme name

--- src/components/Themeing/ThemeProvider/index.js
+++ src/components/Themeing/ThemeProvider/index.js
@@ -12,90 +12,17 @@ import {
 import { Header, Menu } from "../../Navigation";
 import { Provider, useLocalStorage } from "../../Hooks";
 import { Message } from "../../../containers";
-
-export const lightTheme = {
-  name: "light",
-  bg: "#fcfcfc",
-  base: "#fff",
-  accent: "#03a9f3",
-  hover: "#0f7ae5",
-  secondary: "#282c34",
-  tertiary: "#282c34" /*1c2022 */,
-  alt: "#03a9f3",
-  altHover: "#0f7ae5",
-  button: {
-    primary: {
-      color: "#eee",
-      bg: "transparent",
-      border: "#03a9f3",
-      colorHover: "#fcfcfc",
-      bgHover: "transparent",
-      borderHover: "#fcfcfc"
-    },
-    secondary: {
-      color: "#ffffff",
-      bg: "#f56342",
-      border: "#f56342",
-      colorHover: "#ffffff",
-      bgHover: "#be391c",
-      borderHover: "transparent"
-    },
-    default: {
-      color: "#03a9f3",
-      bg: "transparent",
-      border: "#03a9f3",
-      colorHover: "#0f7ae5",
-      bgHover: "transparent",
-      borderHover: "#0f7ae5"
-    }
-  }
-};
-
-export const darkTheme = {
-  name: "dark",
-  bg: "#1c2022",
-  base: "#353535",
-  accent: "#03a9f3",
-  hover: "#5a5a5a",
-  secondary: "#4a4a4a",
-  tertiary: "#fff",
-  alt: "#888",
-  altHover: "#eee",
-  button: {
-    primary: {
-      color: "#a5a5a5",
-      bg: "transparent",
-      border: "#a5a5a5",
-      colorHover: "#e4e4e4",
-      bgHover: "transparent",
-      borderHover: "#e4e4e4"
-    },
-    secondary: {
-      color: "#eee",
-      bg: "#6441a4",
-      border: "#6441a4",
-      colorHover: "#eee",
-      bgHover: "#7d5bbe",
-      borderHover: "transparent"
-    },
-    default: {
-      color: "#a5a5a5",
-      bg: "transparent",
-      border: "#a5a5a5",
-      colorHover: "#e4e4e4",
-      bgHover: "transparent",
-      borderHover: "#e4e4e4"
-    }
-  }
-};
+import { themes } from "../Themes";
 
 const Theme = ({ children }) => {
-  const [selectedTheme, setTheme] = useLocalStorage("theme", lightTheme);
+  const [selectedTheme, setTheme] = useLocalStorage("theme", { name: "light" });
   const [showMenu, setMenu] = useState(false);
 
   const toggleTheme = useCallback(
     () => {
-      setTheme(selectedTheme.name === "dark" ? lightTheme : darkTheme);
+      setTheme(
+        selectedTheme.name === "dark" ? { name: "light" } : { name: "dark" }
+      );
     },
     [selectedTheme.name]
   );
@@ -109,7 +36,7 @@ const Theme = ({ children }) => {
   }, []);
 
   return (
-    <ThemeProvider theme={selectedTheme}>
+    <ThemeProvider theme={themes[selectedTheme.name || "dark"]}>
       <>
         <GlobalStyles />
         <Header onToggleTheme={toggleTheme} />
---
+++ src/components/Themeing/Themes/index.js
@@ -0,0 +1,76 @@
+export const themes = {
+  dark: {
+    name: "dark",
+    bg: "#1c2022",
+    base: "#353535",
+    accent: "#03a9f3",
+    hover: "#5a5a5a",
+    secondary: "#3c3c3c",
+    tertiary: "#fff",
+    alt: "#888",
+    altHover: "#eee",
+    button: {
+      primary: {
+        color: "#a5a5a5",
+        bg: "transparent",
+        border: "#a5a5a5",
+        colorHover: "#e4e4e4",
+        bgHover: "transparent",
+        borderHover: "#e4e4e4"
+      },
+      secondary: {
+        color: "#eee",
+        bg: "#6441a4",
+        border: "#6441a4",
+        colorHover: "#eee",
+        bgHover: "#7d5bbe",
+        borderHover: "transparent"
+      },
+      default: {
+        color: "#a5a5a5",
+        bg: "transparent",
+        border: "#a5a5a5",
+        colorHover: "#e4e4e4",
+        bgHover: "transparent",
+        borderHover: "#e4e4e4"
+      }
+    }
+  },
+  light: {
+    name: "light",
+    bg: "#fcfcfc",
+    base: "#fff",
+    accent: "#03a9f3",
+    hover: "#0f7ae5",
+    secondary: "#282c34",
+    tertiary: "#282c34" /*1c2022 */,
+    alt: "#03a9f3",
+    altHover: "#0f7ae5",
+    button: {
+      primary: {
+        color: "#eee",
+        bg: "transparent",
+        border: "#03a9f3",
+        colorHover: "#fcfcfc",
+        bgHover: "transparent",
+        borderHover: "#fcfcfc"
+      },
+      secondary: {
+        color: "#ffffff",
+        bg: "#f56342",
+        border: "#f56342",
+        colorHover: "#ffffff",
+        bgHover: "#be391c",
+        borderHover: "transparent"
+      },
+      default: {
+        color: "#03a9f3",
+        bg: "transparent",
+        border: "#03a9f3",
+        colorHover: "#0f7ae5",
+        bgHover: "transparent",
+        borderHover: "#0f7ae5"
+      }
+    }
+  }
+};
