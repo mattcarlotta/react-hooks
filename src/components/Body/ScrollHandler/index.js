5f7c1f105b50835ab40a9fb2c8141c9285f00e8e
Matt <carlotta.matt@gmail.com>
2019-05-21 23:09:04 -0700

    Switch to scroll hook

--- src/components/Body/ScrollHandler/index.js
+++ src/components/Body/ScrollHandler/index.js
@@ -1,77 +1,28 @@
-import { PureComponent } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 
-class ScrollHandler extends PureComponent {
-  componentDidMount = () => this.handleScroll();
+const ScrollHandler = ({ location: { hash }, children }) => {
+  useEffect(
+    () => {
+      const element = document.getElementById(hash.replace("#", ""));
 
-  componentDidUpdate = prevProps => {
-    const {
-      location: { pathname, hash }
-    } = this.props;
-    if (
-      pathname !== prevProps.location.pathname ||
-      hash !== prevProps.location.hash
-    ) {
-      this.handleScroll();
-    }
-  };
+      setTimeout(() => {
+        window.scrollTo({
+          behavior: element ? "smooth" : "auto",
+          top: element ? element.offsetTop : 0
+        });
+      }, 100);
+    },
+    [hash]
+  );
 
-  handleScroll = () => {
-    const {
-      location: { hash }
-    } = this.props;
-    const element = document.getElementById(hash.replace("#", ""));
-
-    setTimeout(() => {
-      window.scrollTo({
-        behavior: element ? "smooth" : "auto",
-        top: element ? element.offsetTop : 0
-      });
-    }, 100);
-  };
-
-  render = () => this.props.children;
-}
+  return children;
+};
 
 ScrollHandler.propTypes = {
   children: PropTypes.node.isRequired,
-  location: PropTypes.shape({
-    hash: PropTypes.string,
-    key: PropTypes.string,
-    pathname: PropTypes.string,
-    search: PropTypes.string,
-    state: PropTypes.any
-  })
+  hash: PropTypes.string
 };
 
 export default withRouter(ScrollHandler);
-
-// import { useEffect } from "react";
-// import PropTypes from "prop-types";
-// import { withRouter } from "react-router-dom";
-//
-// const ScrollHandler = ({ location: { hash }, children }) => {
-//   useEffect(
-//     () => {
-//       const element = document.getElementById(hash.replace("#", ""));
-//
-//       setTimeout(() => {
-//         window.scrollTo({
-//           behavior: element ? "smooth" : "auto",
-//           top: element ? element.offsetTop : 0
-//         });
-//       }, 100);
-//     },
-//     [hash]
-//   );
-//
-//   return children;
-// };
-//
-// ScrollHandler.propTypes = {
-//   children: PropTypes.node.isRequired,
-//   hash: PropTypes.string
-// };
-//
-// export default withRouter(ScrollHandler);
